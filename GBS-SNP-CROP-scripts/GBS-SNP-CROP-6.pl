3de031d0dd121488644eb7fb3ab0838e1200183d
arthurmelo <arthurmelobio@gmail.com>
2015-12-22 15:40:37 -0500

    Update GBS-SNP-CROP-6.pl

--- GBS-SNP-CROP-scripts/GBS-SNP-CROP-6.pl
+++ GBS-SNP-CROP-scripts/GBS-SNP-CROP-6.pl
@@ -8,7 +8,7 @@ use strict;
 use warnings;
 use Getopt::Long qw(GetOptions);
 
-my $Usage = "Usage: perl GBS-SNP-CROP-6.pl -b <barcodesID file> -out <output variant master matrix file>\n";
+my $Usage = "Usage: perl GBS-SNP-CROP-6.pl -b <barcodesID file> -out <SNPs master matrix file>\n";
 my $Manual = "Please see Additional File 2 (User Manual) from Melo et al. (2015) BMC Bioinformatics. DOI XXX\n"; 
 
 my ($barcodesID_file,$output_file);
@@ -18,39 +18,40 @@ GetOptions(
 'out=s' => \$output_file,      # file
 ) or die "$Usage\n$Manual\n";
 
+print "\n#######################\n# GBS-SNP-CROP, Step 6\n#######################\n";
+
 my @files = ();
 
-open IN, "$barcodesID_file" or die "Can't find $barcodesID_file file\n";
-	
-while(<IN>) {
+open my $IN, "<", "$barcodesID_file" or die "Can't find $barcodesID_file file\n";
+
+while(<$IN>) {
 	my $barcodesID = $_;
 	chomp $barcodesID;
 	my @barcode = split("\t", $barcodesID);
 	my $barcode_list = $barcode[0];
 	my $TaxaNames = $barcode[1];
-	
+
 	push @files, $TaxaNames;
 }
-
+close $IN;
 chomp (@files);
 
 foreach my $file (@files) {
-    my $mpileup_input = join (".", "$file","mpileup");
+	my $mpileup_input = join (".", "$file","mpileup");
 	my $count_out = join (".", "$file","count","txt");
-			
-	print "\n\nParsing $mpileup_input file...\n";		
-	
-	open (PILEUP, "$mpileup_input") || die "cant load $mpileup_input file";
-	open (OUT1, ">$count_out") || die "cant load file";
-		
-	while (<PILEUP>){
+
+	print "\nParsing $mpileup_input file...\n";
+
+	open my $PILEUP, "<", "$mpileup_input" or die "Can't load $mpileup_input file";
+	open my $OUT1, ">", "$count_out" or die "Can't initialized $count_out ouput file";
+
+	while (<$PILEUP>){
 		my @input1 = split("\t", $_);
 		my $ref = $input1[2];
 		my $algn = uc $input1[4];
 		$algn =~ s/\,/\./g;
 		$algn =~ s/\^.\././g;
-	
-	### Capturing Indels and put it out from mpileup string
+
 		my @positions;
 		my @sizes;
 		my @Indels;
@@ -69,118 +70,93 @@ foreach my $file (@files) {
 			$algn = join ("", "$start", "$end" );
 		}
 
-		### Count Indels frequency and store a hash with Indel type and count	
-		# my %Indel_cnt;
-		# $Indel_cnt{$_}++ foreach @Indels;
-		
-		# my @IndelType = sort {$Indel_cnt{$b} <=> $Indel_cnt{$a}} keys %Indel_cnt;
-		# my @IndelCount = @Indel_cnt{@IndelType};
-	
-		### Work on a nucleotides specific string
 		$algn =~ s/\$//g;   
 		$algn =~ s/\*//g;
 		$algn =~ s/\./$ref/g;
-		
+
 		my @bases = split(//, $algn);
-		@bases = grep /\S/, @bases; # remove empty elements from array
+		@bases = grep /\S/, @bases;
+
+		my $A=0;
+		my $C=0; 
+		my $G=0;
+		my $T=0; 
 		
-		my $A=0;	my $C=0; 
-		my $G=0;	my $T=0; 
-		
-        	for(my $x=0; $x<scalar(@bases);$x++) {
+		for(my $x=0; $x<scalar(@bases);$x++) {
 			if ($bases[$x] =~ /A/){
-                   	$A++;
+					$A++;
 			}
-            		if ($bases[$x] =~ /C/){
-                	 $C++;
-            		}
-            		if ($bases[$x] =~ /G/){
-                	    $G++;
-            		}
-            		if ($bases[$x] =~ /T/){
-                	    $T++;
-        		}
-        	}
-        	
-        	### Call only SNPs
-        	print OUT1 join ("\t",$input1[0],$input1[1],$input1[2]),"\t",join(",","$A","$C","$G","$T"),"\n";
-        	
-        	
-        	### Call SNPs and Indels. Needs improvements ...
-#		if (scalar @IndelCount == 0) { 	
-#			print OUT1 join ("\t",$input1[0],$input1[1],$input1[2]),"\t",join(",","$A","$C","$G","$T","_","_","_","_"),"\n";
-#			next;
-#		} elsif (scalar @IndelCount == 1) {
-#			print OUT1 join ("\t",$input1[0],$input1[1],$input1[2]),"\t",join(",","$A","$C","$G","$T","$IndelCount[0]","$IndelType[0]","_","_"),"\n";
-#			next;
-#		} elsif (scalar @IndelCount > 1) {
-#			print OUT1 join ("\t",$input1[0],$input1[1],$input1[2]),"\t",join(",","$A","$C","$G","$T","$IndelCount[0]","$IndelType[0]","$IndelCount[1]","$IndelType[1]"),"\n";
-#			next;
-#		} else {
-#			next;
-#		}
+			if ($bases[$x] =~ /C/){
+					$C++;
+			}
+			if ($bases[$x] =~ /G/){
+					$G++;
+			}
+			if ($bases[$x] =~ /T/){
+			$T++;
+			}
+		}
+
+		print $OUT1 join ("\t",$input1[0],$input1[1],$input1[2]),"\t",join(",","$A","$C","$G","$T"),"\n";
 	}
-	
-	close PILEUP;
-	close OUT1;
-	
-	print "Filtering polymorphic sites from $count_out file...\n";
-	
+
+	close $PILEUP;
+	close $OUT1;
+
+	print "Filtering monomorphic sites from $count_out file...\n";
+
 	my $countF_out = join (".", "$file","countF","txt");
 	my $ref_file = join (".", "$file","ref","txt");
-		
-	open (COUNT, "$count_out") || die "Unable to identify $count_out\n";
-	open (REF, ">$ref_file") || die "Unable to identify $ref_file\n";
-	open (OUT2, ">$countF_out") || die "cant load file";
-	
-	my @output;
-	while(<COUNT>) {
-		my @input2 = split "\t", $_;
-        chomp @input2;
-        my $ref = $input2[2];
-        my @geno = split(",", $input2[3]);
-	    
-	if ($ref eq "A" and $geno[0] == 0 and ($geno[1]!= 0 or $geno[2] != 0 or $geno[3] != 0) or
-		$ref eq "A" and $geno[0] != 0 and ($geno[1]!= 0 or $geno[2] != 0 or $geno[3] != 0) or
-     		$ref eq "A" and $geno[0] != 0 and ($geno[1]!= 0 or $geno[2] != 0 or $geno[3] != 0) or
-	        $ref eq "C" and $geno[1] == 0 and ($geno[0]!= 0 or $geno[2] != 0 or $geno[3] != 0) or
-    		$ref eq "C" and $geno[1] != 0 and ($geno[0]!= 0 or $geno[2] != 0 or $geno[3] != 0) or
-       		$ref eq "G" and $geno[2] == 0 and ($geno[0]!= 0 or $geno[1] != 0 or $geno[3] != 0) or
-         	$ref eq "G" and $geno[2] != 0 and ($geno[0]!= 0 or $geno[1] != 0 or $geno[3] != 0) or   	
-        	$ref eq "T" and $geno[3] == 0 and ($geno[0]!= 0 or $geno[1] != 0 or $geno[2] != 0) or
-    		$ref eq "T" and $geno[3] != 0 and ($geno[0]!= 0 or $geno[1] != 0 or $geno[2] != 0) ) {
 
-       		@output = join ("\t", @input2);
-    		print OUT2 "@output\n";
-    		print REF join ("\t", $input2[0], $input2[1], $input2[2]),"\n";
-    		}
-	}
-	print "DONE.\n";
-	close COUNT;
-	close OUT2;
+	open my $COUNT, "<", "$count_out" or die "Unable to identify $count_out\n";
+	open my $REF, ">", "$ref_file" or die "Unable to identify $ref_file\n";
+	open my $OUT2, ">", "$countF_out" or die "Can't initialized $countF_out output file";
+
+	my @output;
+	while(<$COUNT>) {
+		my @input2 = split "\t", $_;
+		chomp @input2;
+		my $ref = $input2[2];
+		my @geno = split(",", $input2[3]);
+
+		if ($ref eq "A" and $geno[0] == 0 and ($geno[1]!= 0 or $geno[2] != 0 or $geno[3] != 0) or
+		$ref eq "A" and $geno[0] != 0 and ($geno[1]!= 0 or $geno[2] != 0 or $geno[3] != 0) or
+		$ref eq "C" and $geno[1] == 0 and ($geno[0]!= 0 or $geno[2] != 0 or $geno[3] != 0) or
+		$ref eq "C" and $geno[1] != 0 and ($geno[0]!= 0 or $geno[2] != 0 or $geno[3] != 0) or
+		$ref eq "G" and $geno[2] == 0 and ($geno[0]!= 0 or $geno[1] != 0 or $geno[3] != 0) or
+		$ref eq "G" and $geno[2] != 0 and ($geno[0]!= 0 or $geno[1] != 0 or $geno[3] != 0) or
+		$ref eq "T" and $geno[3] == 0 and ($geno[0]!= 0 or $geno[1] != 0 or $geno[2] != 0) or
+		$ref eq "T" and $geno[3] != 0 and ($geno[0]!= 0 or $geno[1] != 0 or $geno[2] != 0) ) {
+
+			@output = join ("\t", @input2);
+			print $OUT2 "@output\n";
+			print $REF join ("\t", $input2[0], $input2[1], $input2[2]),"\n";
+			}
+		}
+		print "DONE.\n";
+		close $COUNT;
+		close $OUT2;
 }
 
-print "\nCreating a polymorphic variant list...\n";
+print "\nCreating a master list of all putative SNP positions in the population...\n";
 
 system ( "cat *.ref.txt | uniq > VerticalRefPos.txt" );
-
-# Creating a count file list
 system ( "ls *.count.txt > CountFileList.txt" );
 
 print "DONE.\n";
 
-print "\nCreating a variant depth count master matrix...\n";
+print "\nCreating a comprehensive master matrix, with genotype-specific alignment summaries,for all putative SNPs in the population ...\n";
 
 my $posFile = "VerticalRefPos.txt";
 my $countList = "CountFileList.txt";
 
-open (POS, "$posFile") || die "cant load file $!";
-open (LIST, "$countList") || die "cant load file $!";
-open (DEST, ">$output_file") || die "cant load file $!";
+open my $POS, "<", "$posFile" || die "Can't load file $!";
+open my $LIST, "<","$countList" || die "Can't load file $!";
+open my $DEST, ">", "$output_file" || die "Can't load file $!";
 
 my %posHash;
 
-while (<POS>){
+while (<$POS>){
 	chomp;
 	my @input4 = split("\t", $_);
 	my $chr_pos_ref = join("\t", "$input4[0]", "$input4[1]", "$input4[2]");
@@ -192,18 +168,19 @@ while (<POS>){
 	}
 
 }
-close POS;
+close $POS;
 
-while ( my $fileName = <LIST> ) {
+while ( my $fileName = <$LIST> )
+{
 	chomp $fileName;
-	open (GENO_BASE_COUNT_FILE, "$fileName") or die "can't load file $!";
+	open my $GENO_BASE_COUNT_FILE, "<", "$fileName" or die "Can't load file $!";
 
 	my %genoHash;
 
-	while ( my $line = <GENO_BASE_COUNT_FILE>){
+	while ( my $line = <$GENO_BASE_COUNT_FILE>){
 		my @input5 = split ("\t", $line);
 		chomp @input5;
-		my $chr_pos_ref = join ("\t", "$input5[0]", "$input5[1]", "$input5[2]");
+		my $chr_pos_ref = join ("\t","$input5[0]", "$input5[1]","$input5[2]");
 		$genoHash{$chr_pos_ref} = $input5[3];
 		@input5 = ();
 	}
@@ -217,36 +194,32 @@ while ( my $fileName = <LIST> ) {
 	}
 
 	%genoHash = ();
-	close GENO_BASE_COUNT_FILE;
+	close $GENO_BASE_COUNT_FILE;
 }
 
-close LIST;
+close $LIST;
 
 foreach my $key ( sort {(split /\t/, $a)[0] cmp (split /\t/, $b)[0] || (split /\t/, $a)[1] <=> (split /\t/, $b)[1]} keys %posHash ) {
-	print DEST "$posHash{$key}\n";
+	print $DEST "$posHash{$key}\n";
 }
 
-close DEST;
+close $DEST;
 
 print "DONE.\n\n";
 
 sub main {
-   	my $dir = "mpileup"; 
-   	unless(-e $dir, or mkdir $dir) {die "Directory $dir just exist.\n";}
-   	}
+	my $dir = "mpileup"; 
+	unless(-e $dir, or mkdir $dir) {die "Directory $dir does not exist and cannot be created.\n";}
+	}
 main();
 
-system ( "mv *.count.txt ./mpileup" );
-system ( "mv *.mpileup ./mpileup" );
-system ( "mv VerticalRefPos.txt ./mpileup" );
-system ( "mv CountFileList.txt ./mpileup" );
-system ( "rm *.ref.txt" );
-system ( "rm *.countF.txt" );
+system ( "mv *.count.txt *.mpileup VerticalRefPos.txt CountFileList.txt ./mpileup" );
+system ( "rm *.ref.txt *.countF.txt" );
 
-print "The variant count master matrix was successfully created.\nPlease, proceed with filtering and genotyping call on step 7.\n";
+print "The master matrix was successfully created.\nPlease, proceed with SNP filtering and genotyping.\n";
 
-print "\nPlease cite: Melo et al. (2015) GBS-SNP-CROP: A reference-optional pipeline for
-SNP discovery and plant germplasm characterization using variable length, paired-end
-genotyping-by-sequencing data. BMC Bioinformatics. DOI XXX.\n\n";
+print "\n\nPlease cite: Melo et al. (2015) GBS-SNP-CROP: A reference-optional pipeline for\n"
+."SNP discovery and plant germplasm characterization using variable length, paired-end\n"
+."genotyping-by-sequencing data. BMC Bioinformatics. DOI XXX.\n\n";
 
 exit;
