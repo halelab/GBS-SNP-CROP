ef69de3a4d9df8f93fc4542233b2a73537fbec9a
arthurmelo <arthurmelobio@gmail.com>
2015-12-22 15:25:53 -0500

    Update GBS-SNP-CROP-2.pl

--- GBS-SNP-CROP-scripts/GBS-SNP-CROP-2.pl
+++ GBS-SNP-CROP-scripts/GBS-SNP-CROP-2.pl
@@ -13,33 +13,35 @@ use strict;
 use warnings;
 use Getopt::Long qw(GetOptions);
 
-my $Usage = "Usage: perl GBS-SNP-CROP-2.pl -fq <FASTQ file name seed> -t <threads> -ph <Phred quality score: 33 or 64> 
--l <Trimmomatic LEADING value> -sl <Trimmomatic SLIDINGWINDOW value> -tr <Trimmomatic TRAILING value> -m <Trimmomatic MINLEN value>\n";
+my $Usage = "Usage: perl GBS-SNP-CROP-2.pl -fq <FASTQ file name seed> -t <number of threads> -ph <Phred quality score: 33 or 64>\n" 
+."-l <Trimmomatic LEADING value> -sl <Trimmomatic SLIDINGWINDOW value> -tr <Trimmomatic TRAILING value> -m <Trimmomatic MINLEN value>.\n";
 my $Manual = "Please see Additional File 2 (User Manual) from Melo et al. (2015) BMC Bioinformatics. DOI XXX\n"; 
 
 my ($fastq_seed,$threads,$phred,$leading,$sliding,$trailing,$minlen);
 
 GetOptions(
-'fq=s' => \$fastq_seed,       # string
-'t=s' => \$threads,			  # numeric 
-'ph=s' => \$phred,			  # numeric
-'l=s' => \$leading,			  # numeric
-'sl=s' => \$sliding,			  # numeric
-'tr=s' => \$trailing,		  # numeric
-'m=s' => \$minlen,			  # numeric 
+'fq=s' => \$fastq_seed,   # string
+'t=s' => \$threads,       # numeric
+'ph=s' => \$phred,        # numeric
+'l=s' => \$leading,       # numeric
+'sl=s' => \$sliding,      # numeric
+'tr=s' => \$trailing,     # numeric
+'m=s' => \$minlen,        # numeric
 ) or die "$Usage\n$Manual\n";
 
-print "\n\nConcatenating library $fastq_seed R1 reads...";
-my $outR1 = join ("","$fastq_seed","_R1parsed",".fastq");
-system ( "cat $fastq_seed*R1parsed.fastq > $outR1" );
+print "\n#######################\n# GBS-SNP-CROP, Step 2\n#######################\n";
+
+print "\nConcatenating library $fastq_seed R1 reads...";
+my $outR1 = join ("","$fastq_seed","_R1parsed",".fastq",".gz");
+system ( "cat $fastq_seed*R1parsed.fastq.gz > $outR1" );
 print "DONE.";
 
-print "\n\nConcatenating library $fastq_seed R2 reads...";
-my $outR2 = join ("","$fastq_seed","_R2parsed",".fastq");
-system ( "cat $fastq_seed*R2parsed.fastq > $outR2" );
+print "\nConcatenating library $fastq_seed R2 reads...";
+my $outR2 = join ("","$fastq_seed","_R2parsed",".fastq",".gz");
+system ( "cat $fastq_seed*R2parsed.fastq.gz > $outR2" );
 print "DONE.";
 
-print "\n\nTrimmomatic paire-end reads cleanning start...\n";
+print "\nTrimmomatic paired-end reads cleanning/trimming initiated...\n";
 
 my $trimoPER1OUT = join("","$fastq_seed","_PE_R1parsed",".fastq");
 my $trimoSRR1OUT = join("","$fastq_seed","_SR_R1parsed",".fastq");
@@ -48,10 +50,10 @@ my $trimoSRR2OUT = join("","$fastq_seed","_SR_R2parsed",".fastq");
 
 system ( "java -jar /usr/local/bin/trimmomatic-0.33.jar PE -threads $threads -phred$phred $outR1 $outR2 $trimoPER1OUT $trimoSRR1OUT $trimoPER2OUT $trimoSRR2OUT LEADING:$leading SLIDINGWINDOW:$sliding TRAILING:$trailing MINLEN:$minlen" );
 
-print "\nRead trimming completed.\n";
+print "\nTrimmomatic paired-end reads cleaning/trimming completed.\n";
 
-print "\n\nPlease cite: Melo et al. (2015) GBS-SNP-CROP: A reference-optional pipeline for
-SNP discovery and plant germplasm characterization using variable length, paired-end
-genotyping-by-sequencing data. BMC Bioinformatics. DOI XXX.\n\n";
+print "\n\nPlease cite: Melo et al. (2015) GBS-SNP-CROP: A reference-optional pipeline for\n"
+."SNP discovery and plant germplasm characterization using variable length, paired-end\n"
+."genotyping-by-sequencing data. BMC Bioinformatics. DOI XXX.\n\n";
 
 exit;
