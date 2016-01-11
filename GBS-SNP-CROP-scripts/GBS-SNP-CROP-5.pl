8e0c1b7ee3076472cf0a0244ae28900659e55c5c
arthurmelo <arthurmelobio@gmail.com>
2016-01-11 09:28:19 -0500

    Update GBS-SNP-CROP-5.pl

--- GBS-SNP-CROP-scripts/GBS-SNP-CROP-5.pl
+++ GBS-SNP-CROP-scripts/GBS-SNP-CROP-5.pl
@@ -15,7 +15,7 @@ use Getopt::Long qw(GetOptions);
 
 my $Usage = "Usage: perl GBS-SNP-CROP-5.pl -b <barcode-ID file name>  -ref <reference FASTA file> -Q <Phred score> -q <mapping quality score>\n"
 ." -f <SAMTools -f flag> -F <SAMTools _F flag> -t <threads> -Opt <any additional desired SAMTools options>.\n";
-my $Manual = "Please see Additional File 2 (User Manual) from Melo et al. (2015) BMC Bioinformatics. DOI XXX\n"; 
+my $Manual = "Please see Additional File 2 (User Manual) from Melo et al. (2015) BMC Bioinformatics. DOI 10.1186/s12859-016-0879-y\n"; 
 
 my ($barcodesID_file,$Reference,$phred_Q,$map_q,$f,$F,$threads,$sam_add);
 
@@ -136,6 +136,6 @@ system ( "rm *.sam" );
 
 print "\n\nPlease cite: Melo et al. (2015) GBS-SNP-CROP: A reference-optional pipeline for\n"
 ."SNP discovery and plant germplasm characterization using variable length, paired-end\n"
-."genotyping-by-sequencing data. BMC Bioinformatics. DOI XXX.\n\n";
+."genotyping-by-sequencing data. BMC Bioinformatics. DOI 10.1186/s12859-016-0879-y.\n\n";
 
 exit;
