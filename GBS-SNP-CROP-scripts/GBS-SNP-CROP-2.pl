ed9a1ffa0f85fa82594a19d3ddcee6d7279a18f7
arthurmelo <arthurmelobio@gmail.com>
2016-01-11 09:27:08 -0500

    Update GBS-SNP-CROP-2.pl

--- GBS-SNP-CROP-scripts/GBS-SNP-CROP-2.pl
+++ GBS-SNP-CROP-scripts/GBS-SNP-CROP-2.pl
@@ -15,7 +15,7 @@ use Getopt::Long qw(GetOptions);
 
 my $Usage = "Usage: perl GBS-SNP-CROP-2.pl -fq <FASTQ file name seed> -t <number of threads> -ph <Phred quality score: 33 or 64>\n" 
 ."-l <Trimmomatic LEADING value> -sl <Trimmomatic SLIDINGWINDOW value> -tr <Trimmomatic TRAILING value> -m <Trimmomatic MINLEN value>.\n";
-my $Manual = "Please see Additional File 2 (User Manual) from Melo et al. (2015) BMC Bioinformatics. DOI XXX\n"; 
+my $Manual = "Please see Additional File 2 (User Manual) from Melo et al. (2015) BMC Bioinformatics. DOI 10.1186/s12859-016-0879-y\n"; 
 
 my ($fastq_seed,$threads,$phred,$leading,$sliding,$trailing,$minlen);
 
@@ -54,6 +54,6 @@ print "\nTrimmomatic paired-end reads cleaning/trimming completed.\n";
 
 print "\n\nPlease cite: Melo et al. (2015) GBS-SNP-CROP: A reference-optional pipeline for\n"
 ."SNP discovery and plant germplasm characterization using variable length, paired-end\n"
-."genotyping-by-sequencing data. BMC Bioinformatics. DOI XXX.\n\n";
+."genotyping-by-sequencing data. BMC Bioinformatics. DOI 10.1186/s12859-016-0879-y.\n\n";
 
 exit;
